0c04db3f02031d8bee7ba2ea683057fb7d6a017e
lee <maxlee@163.com>
2020-12-09 11:22:28 +0800

    jinja+vue is amazing!however the structure of this programme is still under consideration

--- main.py
+++ main.py
@@ -1,8 +1,57 @@
 from flask import Flask
 from flask import request
-from flask import render_template
+from flask import render_template ,jsonify
+from flask_cors import CORS
+import pymongo
 
 app=Flask(__name__)
+CORS(app)
+
+myclient=pymongo.MongoClient('mongodb://localhost:27017/')
+mydb=myclient['test']
+mycollection=mydb['blog']
+
+@app.route('/')
+def home():
+    print('home router called')
+    return render_template('home.html')
+
+@app.route('/administer',methods=['GET','POST','DELETE'])
+def administer():
+    if request.method=='GET':
+        return  render_template('administer.html',test='test')
+    elif request.method=='POST':
+        return 'administer posted'
+    else:
+        return 'administer delete'
+
+@app.route('/administer_data')
+def data():
+
+    return {'greeting':'greeting from flask'}
+
+@app.route('/blog')
+def blog():
+    bloglist=[]
+    for blog in mycollection.find({"type" : "blog"}):
+        print(blog)
+        bloglist.append(blog)
+    print(bloglist)
+    return  render_template('blog.html',
+                                normaldata='normal data',
+                                list=[{"context":'blog1'}],
+                                blog=bloglist,
+                            )
+
+@app.route('/recommendation',methods=['GET','POST'])
+def recommenddation():
+    if request.method=='GET':
+        return  render_template('recommendation.html')
+    else:
+        return 'recommendation posted'
+
+
+
 
 # @app.route('/index')
 # def index():
@@ -21,30 +70,3 @@ app=Flask(__name__)
 #         return 'LOGIN POST USERNAME:%s' % username
 #     else:
 #         return render_template('login.html',username=username)
-
-
-@app.route('/')
-def home():
-    print('home router called')
-    return render_template('home.html')
-
-
-@app.route('/administer',methods=['GET','POST','DELETE'])
-def administer():
-    if request.method=='GET':
-        return  render_template('administer.html')
-    elif request.method=='POST':
-        return 'administer posted'
-    else:
-        return 'administer delete'
-
-@app.route('/blog')
-def blog():
-    return  render_template('blog.html')
-
-@app.route('/recommendation',methods=['GET','POST'])
-def recommenddation():
-    if request.method=='GET':
-        return  render_template('recommendation.html')
-    else:
-        return 'recommendation posted'
